1992dda659e63889b2921a3251cad324fa509e37
Edward <you@email.com>
2017-04-27 20:52:49 -0700

    added video

--- front-page.php
+++ front-page.php
@@ -10,8 +10,12 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
             <section class="hero-container">
-                
-            </section>
+				<video autoplay class="video-background">
+  					<source src="<?php echo get_template_directory_uri(); ?>/images/Heaven.mp4" type="video/mp4">
+  					<source src="<?php echo get_template_directory_uri(); ?>/images/Heaven.ogv" type="video/ogg">
+					Your browser does not support the video tag.
+				</video>
+    		</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
