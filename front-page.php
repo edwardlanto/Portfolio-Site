c8edcdfd99aebf756d1ae174c651a5b9837e9ced
Edward <you@email.com>
2017-04-28 20:10:26 -0700

    added responsive menu

--- front-page.php
+++ front-page.php
@@ -9,6 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		
             <section class="hero-container">
 				<div class="header-animation">
 					<span>H</span>
@@ -21,7 +22,7 @@ get_header(); ?>
 					<span>A</span>
 					<span>R</span>
 					<span>D</span>
-  				</div>
+  				</div><!--header-animation-->
 				<div class="hero-paragraph-container">
 					<div class="page-hero">
 						<h1>Your Technical Partner To Web Success</h1>
@@ -32,7 +33,16 @@ get_header(); ?>
   					<source src="<?php echo get_template_directory_uri(); ?>/images/Night-Traffic.mp4" type="video/mp4">
 					Your browser does not support the video tag.
 				</video>
+				<ul class="nav-list">
+					<li>About /</li>
+					<li>Portfolio /</li>
+					<li>Services /</li>
+					<li>Contact</li>
+				</ul>
     		</section>
+			<section class="about-section">
+				<h2 class="section-heading">About Me</h2>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
